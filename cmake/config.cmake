71aef5b90d1e47d6ff1291d756f191617eb99913
Jean-Christophe Pince <jcpince@gmail.com>
2023-05-13 12:37:45 +0200

    Cmake-format.
    Signed-off-by: Jean-Christophe Pince <jcpince@gmail.com>

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,2 +1,6 @@
-set(CONFIG_HWMOCK_SPI ON CACHE INTERNAL "SPI hw support")
-set(CONFIG_HWMOCK_TESTS ON CACHE INTERNAL "hwmock unit tests")
+set(CONFIG_HWMOCK_SPI
+    ON
+    CACHE INTERNAL "SPI hw support")
+set(CONFIG_HWMOCK_TESTS
+    ON
+    CACHE INTERNAL "hwmock unit tests")
